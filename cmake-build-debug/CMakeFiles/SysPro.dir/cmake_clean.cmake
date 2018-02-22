35bf09c3f7a700eef4a2f87568ec11f9d34b005a
siddhesh <21078949+siddheshkand@users.noreply.github.com>
2018-02-22 22:10:21 +0530

    first commit

---
+++ cmake-build-debug/CMakeFiles/SysPro.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SysPro.dir/main.c.o"
+  "SysPro.pdb"
+  "SysPro"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SysPro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SysPro.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SysPro.
+# This may be replaced when dependencies are built.
